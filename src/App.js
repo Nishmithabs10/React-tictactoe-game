997242ac57b2e646c6c5689b7e1c5fb8bac8be74
Nishmithabs10 <132118589+Nishmithabs10@users.noreply.github.com>
2025-01-06 22:23:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Tictactoe from './components/Tictactoe/tictactoe';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Tictactoe/>
+    </div>
+  );
+}
+
+export default App;
